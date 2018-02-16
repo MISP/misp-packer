4bc5ccd9f61f2c25354814a1af7adc852dedbeb8
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-02-16 10:22:50 +0100

    Fixes #2.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -434,11 +434,17 @@ sleep 5
 echo "--- Updating the galaxies… ---"
 sudo -E $PATH_TO_MISP/app/Console/cake userInit -q > /dev/null
 AUTH_KEY=$(mysql -u $DBUSER_MISP -p$DBPASSWORD_MISP misp -e "SELECT authkey FROM users;" | tail -1)
-curl -k -X POST -H "Authorization: $AUTH_KEY" -H "Accept: application/json" -v http://127.0.0.1/galaxies/update > /dev/null 2>&1
-
+echo "--- Updating the galaxies… ---"
+curl --header "Authorization: $AUTH_KEY" --header "Accept: application/json" --header "Content-Type: application/json" -o /dev/null -s -X POST http://127.0.0.1/galaxies/update
 
 echo "--- Updating the taxonomies… ---"
-curl -k -X POST -H "Authorization: $AUTH_KEY" -H "Accept: application/json" -v http://127.0.0.1/taxonomies/update > /dev/null 2>&1
+curl --header "Authorization: $AUTH_KEY" --header "Accept: application/json" --header "Content-Type: application/json" -o /dev/null -s -X POST http://127.0.0.1/taxonomies/update
+
+echo "--- Updating the warning lists… ---"
+curl --header "Authorization: $AUTH_KEY" --header "Accept: application/json" --header "Content-Type: application/json" -o /dev/null -s -X POST http://127.0.0.1/warninglists/update
+
+echo "--- Updating the object templates… ---"
+curl --header "Authorization: $AUTH_KEY" --header "Accept: application/json" --header "Content-Type: application/json" -o /dev/null -s -X POST http://127.0.0.1/objectTemplates/update
 
 
 # echo "--- Enabling MISP new pub/sub feature (ZeroMQ)… ---"

4237475cda15c6f8fe8360b9b7090a4030daaa41
Steve Clement <steve@localhost.lu>
2018-11-19 12:26:26 +0900

    chg: [fix] Reshuffled some lines in rc.local. Typo in json.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -642,14 +642,14 @@ sed -i -e '$i \echo never > /sys/kernel/mm/transparent_hugepage/enabled\n' /etc/
 sed -i -e '$i \echo 1024 > /proc/sys/net/core/somaxconn\n' /etc/rc.local
 sed -i -e '$i \sysctl vm.overcommit_memory=1\n' /etc/rc.local
 sed -i -e '$i \sudo -u www-data bash /var/www/MISP/app/Console/worker/start.sh > /tmp/worker_start_rc.local.log\n' /etc/rc.local
-sed -i -e '$i \sudo -u www-data misp-modules -l 0.0.0.0 -s > /tmp/misp-modules_rc.local.log 2> /dev/null &\n' /etc/rc.local
-sed -i -e '$i \sudo -u www-data bash /var/www/misp-dashboard/start_all.sh > /tmp/misp-dashboard_rc.local.log\n' /etc/rc.local
-sed -i -e '$i \sudo -u misp /usr/local/src/viper/viper-web -p 8888 -H 0.0.0.0 > /tmp/viper-web_rc.local.log &\n' /etc/rc.local
 sed -i -e '$i \git_dirs="/usr/local/src/misp-modules/ /var/www/misp-dashboard /usr/local/src/faup /usr/local/src/mail_to_misp /usr/local/src/misp-modules /usr/local/src/viper /var/www/misp-dashboard"\n' /etc/rc.local
 sed -i -e '$i \for d in $git_dirs; do\n' /etc/rc.local
 sed -i -e '$i \    echo "Updating ${d}" >> /tmp/git-update_rc.local.log\n' /etc/rc.local
 sed -i -e '$i \    cd $d && sudo git pull  >> /tmp/git-update_rc.local.log &\n' /etc/rc.local
 sed -i -e '$i \done\n' /etc/rc.local
+sed -i -e '$i \sudo -u www-data misp-modules -l 0.0.0.0 -s > /tmp/misp-modules_rc.local.log 2> /dev/null &\n' /etc/rc.local
+sed -i -e '$i \sudo -u www-data bash /var/www/misp-dashboard/start_all.sh > /tmp/misp-dashboard_rc.local.log\n' /etc/rc.local
+sed -i -e '$i \sudo -u misp /usr/local/src/viper/viper-web -p 8888 -H 0.0.0.0 > /tmp/viper-web_rc.local.log &\n' /etc/rc.local
 
 
 echo "--- Installing MISP modules… ---"

166041442e006d1061bec7647608234c6a75c472
Steve Clement <steve@localhost.lu>
2019-05-01 23:49:52 +0900

    chg: [tests] Put tests in /tmp

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -12,11 +12,11 @@ apt-get -y clean > /dev/null 2>&1
 
 echo "--- Testing Instance ---"
 cd /var/www/MISP/PyMISP
-/var/www/MISP/venv/bin/python tests/testlive_comprehensive.py 2> /root/tests-output.txt
+/var/www/MISP/venv/bin/python tests/testlive_comprehensive.py 2> /tmp/tests-output.txt
 
 if [ "$?" != "0" ]; then
 ##  set smtp=smtp://149.13.33.5 ; cat /tmp/output.txt |mail -s "tests/testlive_comprehensive.py failed on autogen-VM" steve.clement@circl.lu
-echo "Damage, terrible terrible damage!!!!" >> /root/tests-output.txt
+echo "Damage, terrible terrible damage!!!!" >> /tmp/tests-output.txt
 fi
 
 rm -rf tests/viper-test-files

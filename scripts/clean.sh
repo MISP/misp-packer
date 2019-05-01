0b8c518e5dbc37152b2d59a989a5a5d502041be7
Steve Clement <steve@localhost.lu>
2019-05-01 23:47:36 +0900

    fix: [tests] Tests are setup during the run, this file has a second test.

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -11,11 +11,7 @@ echo "--- Cleaning packages"
 apt-get -y clean > /dev/null 2>&1
 
 echo "--- Testing Instance ---"
-AUTH_KEY=$(cat /tmp/AUTH_KEY.txt)
 cd /var/www/MISP/PyMISP
-sudo -H -u www-data sed -i "s/LBelWqKY9SQyG0huZzAMqiEBl6FODxpgRRXMsZFu/${AUTH_KEY}/g" tests/testlive_comprehensive.py
-sudo -H -u www-data sed -i 's/http:\/\/localhost:8080/https:\/\/localhost/g' tests/testlive_comprehensive.py
-sudo -H -u www-data git clone https://github.com/viper-framework/viper-test-files tests/viper-test-files
 /var/www/MISP/venv/bin/python tests/testlive_comprehensive.py 2> /root/tests-output.txt
 
 if [ "$?" != "0" ]; then
@@ -24,9 +20,8 @@ echo "Damage, terrible terrible damage!!!!" >> /root/tests-output.txt
 fi
 
 rm -rf tests/viper-test-files
-##rm /tmp/output.txt /tmp/AUTH_KEY.txt
+##rm /tmp/output.txt
 
 # End Cleaning
 echo "VM cleaned and rebooting for automagic reas0ns."
 reboot
-

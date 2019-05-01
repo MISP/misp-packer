69521a943f22a28f081616f208bf9b287dadae20
Steve Clement <steve@localhost.lu>
2019-05-01 23:59:09 +0900

    chg: [tests] Better handling and enabled mailing.

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,22 +5,23 @@ mv /tmp/crontab /etc/cron.d/misp
 
 # package
 echo "--- autoremove for apt ---"
-apt-get -y autoremove > /dev/null 2>&1
+apt-get autoremove -qq > /dev/null 2>&1
 
 echo "--- Cleaning packages"
-apt-get -y clean > /dev/null 2>&1
+apt-get clean -qq > /dev/null 2>&1
+
+apt-get install mailutils -qq > /dev/null 2>&1
 
 echo "--- Testing Instance ---"
 cd /var/www/MISP/PyMISP
 /var/www/MISP/venv/bin/python tests/testlive_comprehensive.py 2> /tmp/tests-output.txt
 
 if [ "$?" != "0" ]; then
-##  set smtp=smtp://149.13.33.5 ; cat /tmp/output.txt |mail -s "tests/testlive_comprehensive.py failed on autogen-VM" steve.clement@circl.lu
-echo "Damage, terrible terrible damage!!!!" >> /tmp/tests-output.txt
+  echo "Damage, terrible terrible damage!!!!" >> /tmp/tests-output.txt
+  set smtp=smtp://149.13.33.5 ; cat /tmp/tests-output.txt |mail -s "tests/testlive_comprehensive.py failed on autogen-VM" steve.clement@circl.lu
 fi
 
 rm -rf tests/viper-test-files
-##rm /tmp/output.txt
 
 # End Cleaning
 echo "VM cleaned and rebooting for automagic reas0ns."

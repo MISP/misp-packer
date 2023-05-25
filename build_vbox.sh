e3cc666e9f926c58c12bf68b32191b58cdcb82cc
Rawane Boubrima <rawane.boubrima7@etu.univ-lorraine.fr>
2023-05-25 13:12:11 +0200

    test

--- build_vbox.sh
+++ build_vbox.sh
@@ -13,21 +13,57 @@ fi
 SHA_SUMS="1 256 384 512"
 
 checkInstaller () {
-  for sum in $(echo ${SHA_SUMS}); do
-    /usr/bin/wget -q -O scripts/INSTALL.sh.sha${sum} https://raw.githubusercontent.com/MISP/MISP/2.4/INSTALL/INSTALL.sh.sha${sum}
-    INSTsum=$(shasum -a ${sum} scripts/INSTALL.sh | cut -f1 -d\ )
-    chsum=$(cat scripts/INSTALL.sh.sha${sum} | cut -f1 -d\ )
-
-    if [[ "$chsum" == "$INSTsum" ]]; then
-      echo "sha${sum} matches"
-    else
-      echo "sha${sum}: ${chsum} does not match the installer sum of: ${INSTsum}"
-      echo "Deleting installer, please run again."
-      rm scripts/INSTALL.sh
-      exit 1
+if [[ "${FLAVOUR}" == "rhel" ]] || [[ "${FLAVOUR}" == "centos" ]] || [[ "${FLAVOUR}" == "fedora" ]]; then
+  INSTsum=$(sha512sum ${0} | cut -f1 -d\ )
+  /usr/bin/wget --no-cache -q -O /tmp/INSTALL.sh.sha512 https://raw.githubusercontent.com/MISP/MISP/2.4/INSTALL/INSTALL.sh.sha512
+        chsum=$(cat /tmp/INSTALL.sh.sha512)
+  if [[ "${chsum}" == "${INSTsum}" ]]; then
+    echo "SHA512 matches"
+  else
+    echo "SHA512: ${chsum} does not match the installer sum of: ${INSTsum}"
+    # exit 1 # uncomment when/if PR is merged
+  fi
+  else
+    # TODO: Implement $FLAVOUR checks and install depending on the platform we are on
+    if [[ $(which shasum > /dev/null 2>&1 ; echo $?) -ne 0 ]]; then
+      checkAptLock
+      sudo apt install libdigest-sha-perl -qyy
     fi
-  done
+    # SHAsums to be computed, not the -- notatiation is for ease of use with rhash
+    SHA_SUMS="--sha1 --sha256 --sha384 --sha512"
+    for sum in $(echo ${SHA_SUMS} |sed 's/--sha//g'); do
+      /usr/bin/wget --no-cache -q -O /tmp/INSTALL.sh.sha${sum} https://raw.githubusercontent.com/MISP/MISP/2.4/INSTALL/INSTALL.sh.sha${sum}
+      INSTsum=$(shasum -a ${sum} ${0} | cut -f1 -d\ )
+      chsum=$(cat /tmp/INSTALL.sh.sha${sum} | cut -f1 -d\ )
+
+      if [[ "${chsum}" == "${INSTsum}" ]]; then
+        echo "sha${sum} matches"
+      else
+        echo "sha${sum}: ${chsum} does not match the installer sum of: ${INSTsum}"
+        echo "Delete installer, re-download and please run again."
+        exit 1
+      fi
+    done
+fi
 }
+
+
+#checkInstaller () {
+#  for sum in $(echo ${SHA_SUMS}); do
+#    /usr/bin/wget -q -O scripts/INSTALL.sh.sha${sum} https://raw.githubusercontent.com/MISP/MISP/2.4/INSTALL/INSTALL.sh.sha${sum}
+#    INSTsum=$(shasum -a ${sum} scripts/INSTALL.sh | cut -f1 -d\ )
+#    chsum=$(cat scripts/INSTALL.sh.sha${sum} | cut -f1 -d\ )
+
+#    if [[ "$chsum" == "$INSTsum" ]]; then
+#      echo "sha${sum} matches"
+#    else
+#      echo "sha${sum}: ${chsum} does not match the installer sum of: ${INSTsum}"
+#      echo "Deleting installer, please run again."
+#      rm scripts/INSTALL.sh
+#      exit 1
+#    fi
+#  done
+#}
 
 # Fetch and check installer
 if [[ -f "scripts/INSTALL.sh" ]]; then
